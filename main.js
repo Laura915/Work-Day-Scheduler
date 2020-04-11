3d0d3da32b1c83578b11e8403274c8db383b9c65
Laura <lhernan53112@gmail.com>
2020-04-11 19:44:37 -0400

    added color coordination to time blocks based on time of day.

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 //Current day display 
 $("#currentDay").text(moment().format('MMMM Do YYYY '));
 
+//TextArea Displays color coordination
+$(".row").each(function(index){
+    //get times 
+    var time = parseInt(moment().format('H'));
+    var calTime=$("#hour-value-" +index).data("military");
+
+    //compare times 
+    if (time===calTime){
+        //set correct color 
+       var textArea=$(".textarea").get(index);
+       $(textArea).addClass("present");
+       $(textArea).removeClass("future");  
+    }else if(time<calTime){
+
+    }else{
+        var textArea=$(".textarea").get(index);
+        $(textArea).addClass("past");
+        $(textArea).removeClass("future"); 
+    };
+});
